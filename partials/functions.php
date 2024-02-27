95305b36fb34f56046d206f675bd06663b82f5ff
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-02-27 16:00:58 +0100

    function in functions file

---
+++ partials/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function generatePassword($length) {
+
+    $lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    $uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $numbers = '0123456789';
+    $symbols = '!@#$%^&*()-_+=~`[]{}|;:,.<>?';
+
+    $all_characters = $lowercase . $uppercase . $numbers . $symbols;
+
+    $total_characters = strlen($all_characters);
+
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $random_index = mt_rand(0, $total_characters - 1);
+        $password .= $all_characters[$random_index]; 
+    }
+
+    return $password;
+}
+
+$password_length = isset($_GET['password_length']) ? intval($_GET['password_length']) : '' ;
+
+$password = generatePassword($password_length);
